8985a1c64f8c3b9ed9f1f22716b3d902cff1622a
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-12 22:40:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,9 @@ def fizzbuzz(int)
   if int % 3 == 0 
     "Fizz"
   elsif int % 5 == 0 
-  "Buzz"
-  elsif int % 3,5 == 0
   "FizzBuzz"
-else int % !== 0
-  puts "FizzBuzz"
+  else int % 5 == 0
+  "Buzz"
 end
   
 end
