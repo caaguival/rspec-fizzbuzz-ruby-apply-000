d69dfbcc3cb7ca3afe82674a4b18ae9851c1ce91
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-12 22:30:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
-    "fizz"
+  if int % 3 == 0 "Fizz"
   end
-  if int % 5 == 0
-    "buzz"
+  if int % 5 == 0 "Buzz"
   end
   
 end
